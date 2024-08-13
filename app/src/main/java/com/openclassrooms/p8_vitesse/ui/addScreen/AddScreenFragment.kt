e8445b18eb3fb85cd462c7d095295eec3f3ed11a
Nicolas Cahorel <nicolas.cahorel@gmail.com>
2024-08-13 15:03:28 +0200

    fragments and layouts added to project

---
+++ app/src/main/java/com/openclassrooms/p8_vitesse/ui/addScreen/AddScreenFragment.kt
@@ -0,0 +1,26 @@
+package com.openclassrooms.p8_vitesse.ui.addScreen
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.openclassrooms.p8_vitesse.R
+
+
+class AddScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
